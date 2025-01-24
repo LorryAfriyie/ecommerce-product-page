d6b0b37a0d58fe15e4a14990d5a66264236fb0e4
Lawrence Afriyie <lawrenceafriyie35@gmail.com>
2025-01-24 04:21:20 +0200

    Adding custom component and project images

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import "./App.css";
+import { Navbar } from "./components/Navbar";
 
 function App() {
-  return <></>;
+  return (
+    <div className="app">
+      <Navbar />
+      <div className="attribution">
+        Challenge by{" "}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+        . Coded by <a href="#">Lorry Afriyie</a>.
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,7 @@
+export function Navbar() {
+  return (
+    <div className="navbar">
+      <p>Navbar</p>
+    </div>
+  );
+}
