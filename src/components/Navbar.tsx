0cbbe5873f91f1eac8b71efccd157539321b0ee6
Lawrence Afriyie <lawrenceafriyie35@gmail.com>
2025-01-25 04:15:32 +0200

    Navbar creation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,29 @@
 export function Navbar() {
   return (
     <div className="navbar">
-      <p>Navbar</p>
+      <div className="navbar-brand">
+        <p>sneakers</p>
+      </div>
+
+      <div className="navbar-items">
+        <ul className="navbar-link">
+          <li>
+            <a href="">Collection</a>
+          </li>
+          <li>
+            <a href="">Men</a>
+          </li>
+          <li>
+            <a href="">Women</a>
+          </li>
+          <li>
+            <a href="">About</a>
+          </li>
+          <li>
+            <a href="">Contact</a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
