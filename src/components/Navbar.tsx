f6cba916fbf0d0a2bd5d6b5ae5ac3b33e5190754
Lawrence Afriyie <lawrenceafriyie35@gmail.com>
2025-01-27 15:58:54 +0200

    Styling navbar for mobile viewport

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,22 @@
 export function Navbar() {
   return (
-    <div className="navbar">
-      <div className="navbar-brand">
-        <p>sneakers</p>
-      </div>
-
+    <nav className="navbar">
       <div className="navbar-items">
+        <svg
+          width="16"
+          height="15"
+          xmlns="http://www.w3.org/2000/svg"
+          className="menu-icon"
+        >
+          <path
+            d="M16 12v3H0v-3h16Zm0-6v3H0V6h16Zm0-6v3H0V0h16Z"
+            fill="#69707D"
+            fill-rule="evenodd"
+          />
+        </svg>
+        <div className="navbar-brand">
+          <h2>sneakers</h2>
+        </div>
         <ul className="navbar-link">
           <li>
             <a href="">Collection</a>
@@ -24,6 +35,11 @@ export function Navbar() {
           </li>
         </ul>
       </div>
-    </div>
+
+      <div className="navbar-cart">
+        <div className="cart">cart here</div>
+        <div className="profile-icon">profile here</div>
+      </div>
+    </nav>
   );
 }
