994cae1abb0130b8ca13a9c797b3d08b2ceef8fb
Lawrence Afriyie <36528903+LorryAfriyie@users.noreply.github.com>
2025-02-06 18:11:37 +0200

    Product details
    Adding product details.

--- src/components/ProductPage.tsx
+++ src/components/ProductPage.tsx
@@ -3,7 +3,27 @@ export function ProductPage() {
     <div className="product-page">
       <div className="product-page__grid">
         <div className="product-page__img-col">Images</div>
-        <div className="product-page__info-col">Information</div>
+
+        <div className="product-page__info-col">
+          <p className="product-page__company-name">Sneaker Company</p>
+          <h1 className="product-page__product-heading">
+            Fall Limited Edition Sneakers
+          </h1>
+
+          <p className="product-page__product-description">
+            These low-profile sneakers are your perfect casual wear companion.
+            Featuring a durable rubber outer sole, they’ll withstand everything
+            the weather can offer.
+          </p>
+
+          <div className="proudct-page__price-block">
+            <p className="product-page__discount-price">$125.00</p>
+
+            <p className="proudct-page__discount-percentage">50% </p>
+
+            <p className="product-page__original-price">$250.00</p>
+          </div>
+        </div>
       </div>
     </div>
   );
