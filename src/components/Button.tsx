dacecc6f55e77c9eeb684fcd04d2aeae967b7803
Lawrence Afriyie <36528903+LorryAfriyie@users.noreply.github.com>
2025-02-16 14:09:00 +0200

    SVG Components
    Creating function components for SVG media.

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,8 +1,18 @@
+import { ReactNode } from "react";
+
 type ButtonType = {
   text: string;
   btnClass: string;
+  icon?: ReactNode;
+  icon2?: ReactNode;
 };
 
-export function Button({ text, btnClass }: ButtonType) {
-  return <button className={btnClass}>{text}</button>;
+export function Button({ text, btnClass, icon, icon2 }: ButtonType) {
+  return (
+    <button className={btnClass}>
+      {icon}
+      {text}
+      {icon2}
+    </button>
+  );
 }
