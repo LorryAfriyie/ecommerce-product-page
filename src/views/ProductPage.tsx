711e5b63d653c868955da64e6680e3bfb5e7ca15
Lawrence Afriyie <36528903+LorryAfriyie@users.noreply.github.com>
2025-02-21 17:09:37 +0200

    Hero Page adjustments
    Changing hero page width.

--- src/views/ProductPage.tsx
+++ src/views/ProductPage.tsx
@@ -10,28 +10,32 @@ export function ProductPage() {
         </div>
 
         <div className="product-page__info-col">
-          <small className="product-page__company-name">Sneaker Company</small>
-          <h1 className="product-page__product-heading">
-            Fall Limited Edition Sneakers
-          </h1>
+          <div className="container">
+            <small className="product-page__company-name">
+              Sneaker Company
+            </small>
+            <h1 className="product-page__product-heading">
+              Fall Limited Edition Sneakers
+            </h1>
 
-          <p className="product-page__product-description">
-            These low-profile sneakers are your perfect casual wear companion.
-            Featuring a durable rubber outer sole, they’ll withstand everything
-            the weather can offer.
-          </p>
+            <p className="product-page__product-description">
+              These low-profile sneakers are your perfect casual wear companion.
+              Featuring a durable rubber outer sole, they’ll withstand
+              everything the weather can offer.
+            </p>
 
-          <div className="proudct-page__price-block">
-            <div className="discount">
-              <p className="discount__discount-price">$125.00</p>
+            <div className="proudct-page__price-block">
+              <div className="discount">
+                <p className="discount__discount-price">$125.00</p>
 
-              <p className="discount__discount-percentage">50%</p>
+                <p className="discount__discount-percentage">50%</p>
+              </div>
+
+              <p className="product-page__original-price">$250.00</p>
             </div>
 
-            <p className="product-page__original-price">$250.00</p>
+            <CartControl />
           </div>
-
-          <CartControl />
         </div>
       </div>
     </div>
