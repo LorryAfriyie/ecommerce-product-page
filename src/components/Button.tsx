2388242f576594d3389d4fa39ad4e2b4b6059f20
Lawrence Afriyie <36528903+LorryAfriyie@users.noreply.github.com>
2025-02-25 14:00:34 +0200

    Recreating quantity button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,18 +1,54 @@
 import { ReactNode } from "react";
 
 type ButtonType = {
-  text: string;
-  btnClass: string;
+  text?: string;
+  btnClass?: string;
   icon?: ReactNode;
-  icon2?: ReactNode;
+  btnFunction?: () => void;
 };
 
-export function Button({ text, btnClass, icon, icon2 }: ButtonType) {
+type QuantityType = {
+  icon?: ReactNode;
+  icon2?: ReactNode;
+  text?: string;
+  btnClass?: string;
+  btnFunction?: () => void;
+};
+
+export function Button({ text, btnClass, icon, btnFunction }: ButtonType) {
   return (
-    <button className={btnClass}>
+    <button className={btnClass} onClick={btnFunction}>
       {icon}
       {text}
-      {icon2}
     </button>
+  );
+}
+
+export function QuantityBtn({
+  icon,
+  icon2,
+  text,
+  btnClass,
+  btnFunction,
+}: QuantityType) {
+  const btnAttributes = [
+    { id: 1, _icon: icon, _btnFunction: btnFunction },
+    { id: 2, _icon: icon2, _btnFunction: btnFunction },
+  ];
+
+  return (
+    <div className={"quantity-btn-container"}>
+      <Button
+        icon={btnAttributes[0]._icon}
+        btnFunction={btnAttributes[0]._btnFunction}
+      />
+
+      <span>{text}</span>
+
+      <Button
+        icon={btnAttributes[1]._icon}
+        btnFunction={btnAttributes[1]._btnFunction}
+      />
+    </div>
   );
 }
--- src/views/CartControl.tsx
+++ src/views/CartControl.tsx
@@ -1,15 +1,11 @@
-import { Button } from "../components/Button";
+import { Button, QuantityBtn } from "../components/Button";
 import { CartIcon, AddIcon, SubtractIcon } from "../components/SVG.tsx";
 
 export function CartControl() {
   return (
     <div className="cart-control">
-      <Button
-        text="3"
-        btnClass="quantity"
-        icon={<SubtractIcon />}
-        icon2={<AddIcon />}
-      />
+      <QuantityBtn icon={<SubtractIcon />} icon2={<AddIcon />} text={"3"} />
+      {/*<Button text="3" btnClass="quantity" icon={<SubtractIcon />} />*/}
 
       <Button text="Add to Cart" btnClass="add-to-cart" icon={<CartIcon />} />
     </div>
